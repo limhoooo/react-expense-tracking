76e8439109a5638ec4c4e01ea4d9f6d6caaa6b0b
lim ho <dlagh123@gmail.com>
2022-01-12 15:11:13 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { title: 'Car 1Insurance', amount: 12944.67, date: new Date(2021, 2, 11), key: Math.random() },
+    { title: 'Car 2Insurance', amount: 22944.67, date: new Date(2021, 1, 12), key: Math.random() },
+    { title: 'Car 3Insurance', amount: 32944.67, date: new Date(2021, 3, 13), key: Math.random() },
+    { title: 'Car 4Insurance', amount: 42944.67, date: new Date(2021, 4, 2), key: Math.random() },
+    { title: 'Car 5Insurance', amount: 42944.67, date: new Date(2021, 5, 22), key: Math.random() },
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <p>
+        This is also visible!
+      </p>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+
+const ExpenseItem = ({ expenses }) => {
+
+    return (
+        <Card className='expense-item'>
+
+            <ExpenseDate date={expenses.date} />
+            <div className='expense-item__description'>
+                <h2>{expenses.title}</h2>
+                <div className='expense-item__price'>
+                    ${expenses.amount}
+                </div>
+            </div>
+
+        </Card>
+
+    )
+};
+export default ExpenseItem;
+
+
+
+//export default React.memo(ExpenseItem);
